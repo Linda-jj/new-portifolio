acdebebfc0e124151fe30d572fa828851d90c3fb
lideya <lideyafff@gmail.com>
2026-04-24 22:08:57 +0300

    Fix Netlify config

--- app/componet/Header.jsx
+++ app/componet/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 import {assets} from "../../assets/assets";
 import { motion } from "motion/react"
 
-const Header = () => {
+
+
+
+const Header = ({ isDarkMode, setIsDarkMode }) => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
        <motion.div 
